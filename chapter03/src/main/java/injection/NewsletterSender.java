9a81fe0bfec00cf4299412d52f8663484a0a5705
hans <focus5n@naver.com>
2024-02-11 15:15:30 +0900

    인터페이스에 수정자 및 접근자 설정

---
+++ chapter03/src/main/java/injection/NewsletterSender.java
@@ -0,0 +1,10 @@
+package injection;
+
+public interface NewsletterSender {
+    void setSmtpServer(String smtpServer);
+    String getSmtpServer();
+    void setFromAddress(String fromAddress);
+    String getFromAddress();
+
+    void send();
+}
